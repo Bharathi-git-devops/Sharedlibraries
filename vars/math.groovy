51349cf98f48c20259dc233b870cea75bac52ef3
Bharathi-git-devops <bharathikola099@gmail.com>
2026-07-27 16:22:58 +0530

    create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,12 @@
+def add(a,b)
+{
+  echo "the add of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b)
+{
+  echo " the substraction of ${a} and ${b} is ${a-b}"
+}
+def mult(a,b)
+{
+  echo "the multiplication of ${a} and ${b} is ${a*b}"
+}
